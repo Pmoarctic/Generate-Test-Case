7f56d11faf5bcd64ac7689fcdbfce92b55bf8221
Pomboon Pomboonmee <t4ng1336@gmail.com>
2023-10-09 15:03:36 +0700

    gen 1.0

---
+++ ABCpyramid.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int long_line = 1;
+    int start = 0;
+
+    for(int i=0;i<n;i++)
+    {
+        if(start == long_line)
+        {
+            long_line++;
+            start = 0;
+            cout << endl;
+        }
+        start++;
+        cout << alphabet[i%26] << " ";
+    }
+}
